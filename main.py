0530a10418fa3715a876acdaed9c3204a89728d4
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-09-26 23:08:19 +0530

    Profile remaining

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ import webapp2
 import cgi
 from google.appengine.api import users
 from gaesessions import get_current_session
+import pickle
+
+userbase = {}
 
 html  = """
 	<html>
@@ -28,8 +31,6 @@ html  = """
 	<p> to sign in click <a href = "/sign">here</a> </p>
 	<p> Page Count : %d <p>
 	</html>
-
-
 """
 
 signInHtml = """
@@ -42,8 +43,6 @@ signInHtml = """
 </form>
 </body>
 </html>
-
-
 """
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -56,17 +55,34 @@ class signIn(webapp2.RequestHandler):
 		self.response.write(signInHtml)
 class displayValues(webapp2.RequestHandler):
 	def post(self):
-		if(cgi.escape(self.request.get('username'))) != "":
+		session = get_current_session()
+		username = session.get('username' , "")
+		password = session.get('password', "")
+		username = cgi.escape(self.request.get('username'))
+		password = cgi.escape(self.request.get('password'))
+		#username = session.get('username')
+		if(username) != "" and password != "":
 			self.response.write('<html><body><h3>You wrote : </h3>')
 			self.response.write('<h4>Username  : ')
-			self.response.write(cgi.escape(self.request.get('username')))
+			self.response.write(username)
 			self.response.write('</h4><h4>Password  : ')	
-			self.response.write(cgi.escape(self.request.get('password')))
+			self.response.write(password)
 			self.response.write('</h4></body></html>')
 		else:
-			self.response.write('Empty username')
+			self.response.write('Empty username or password <a href = "/">Go Back</a>')	
+class userProfile(webapp2.RequestHandler):
+	def get(self):
+		session = get_current_session()
+		if(session['username']!="" and session['password'] !=""):
+			self.response.write('Welcome '+ session['username'])
+			self.response.write('<br>Your Password: '+ session['password'])
+		else:
+			self.response.write('User not signed in!!!')	
+			self.response.write('Welcome '+ session['username'])
+			self.response.write('<br>Your Password: '+ session['password'])
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/sign', signIn),
-    ('/display', displayValues)
+    ('/display', displayValues),
+    ('/profile', userProfile)
 ], debug=True)
